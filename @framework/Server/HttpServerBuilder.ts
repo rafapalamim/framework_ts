16301c8d976611751dc60a2059231c296e98e23c
Rafael Palamim <palamimweb@gmail.com>
2024-03-26 22:22:39 -0300

    Create http server builder and refactor index.ts

---
+++ @framework/Server/HttpServerBuilder.ts
@@ -0,0 +1,29 @@
+import Controller from '../Controller/Controller'
+import HttpObservabilityMiddleware from '../Observability/HttpObservabilityMiddleware'
+import HttpMiddleware from './HttpMiddleware'
+import HttpServer from './HttpServer'
+
+export default class HttpServerBuilder {
+
+    constructor(private readonly server: HttpServer) { }
+
+    addRoutes(controllers: Controller[] | Controller): void {
+        const routes = Array.isArray(controllers) ? controllers : [controllers]
+        routes.map((controller) => controller.register(this.server))
+    }
+
+    addMiddlewares(middlewares: HttpMiddleware[] | HttpMiddleware): void {
+        const newMiddlewares = Array.isArray(middlewares) ? middlewares : [middlewares]
+        newMiddlewares.map((middleware) => middleware.register(this.server))
+    }
+
+    addObservabilityMiddlewares(middlewares: HttpObservabilityMiddleware[] | HttpObservabilityMiddleware): void {
+        const newMiddlewares = Array.isArray(middlewares) ? middlewares : [middlewares]
+        newMiddlewares.map((middleware) => middleware.register(this.server))
+    }
+
+    build(): HttpServer {
+        return this.server
+    }
+
+}
--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
-import ExampleController from './app/ExampleModule/ExampleController'
 import PrometheusController from './@framework/Observability/Prometheus/PrometheusController'
 import ExpressHttpServerAdapter from './@framework/Server/Adapters/Express/ExpressHttpServerAdapter'
 import PrometheusCollectRequestDurationEnd from './@framework/Server/Adapters/Express/Observability/PrometheusCollectRequestDurationEnd'
 import PrometheusCollectRequestDurationInit from './@framework/Server/Adapters/Express/Observability/PrometheusCollectRequestDurationInit'
+import HttpServerBuilder from './@framework/Server/HttpServerBuilder'
+import ExampleController from './src/ExampleModule/ExampleController'
 
-const app = new ExpressHttpServerAdapter()
+const appBuilder = new HttpServerBuilder(new ExpressHttpServerAdapter())
+appBuilder.addObservabilityMiddlewares(new PrometheusCollectRequestDurationInit())
+appBuilder.addRoutes([new ExampleController(), new PrometheusController()])
+appBuilder.addObservabilityMiddlewares(new PrometheusCollectRequestDurationEnd())
 
-new PrometheusCollectRequestDurationInit().register(app)
-new ExampleController().register(app)
-new PrometheusController().register(app)
-new PrometheusCollectRequestDurationEnd().register(app)
+const app = appBuilder.build()
 
 app.listen(3000)

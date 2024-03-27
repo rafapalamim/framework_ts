0bf989a7ab130c60fe88236de909d186fdb80b1b
Rafael Palamim <palamimweb@gmail.com>
2024-03-27 02:13:44 -0300

    Add route not found and error handler express middlewares

--- index.ts
+++ index.ts
@@ -4,12 +4,14 @@ import PrometheusCollectRequestDuration from './@framework/Observability/Prometh
 import HttpServerBuilder from './@framework/Server/Builder/HttpServerBuilder'
 import ExampleController from './src/ExampleModule/ExampleController'
 import StartRequestTime from './@framework/Server/Middlewares/StartRequestTime'
+import ExpressErrorHandler from './@framework/Server/Adapters/Express/Middlewares/ExpressErrorHandler'
+import ExpressRouteNotFound from './@framework/Server/Adapters/Express/Middlewares/ExpressRouteNotFound'
 
 const appBuilder = new HttpServerBuilder(new ExpressHttpServerAdapter())
 appBuilder.addMiddlewares([new StartRequestTime()])
 appBuilder.addRoutes([new ExampleController(), new PrometheusController()])
+appBuilder.addMiddlewares([new ExpressRouteNotFound(), new ExpressErrorHandler()])
 appBuilder.addObservabilityMiddlewares(new PrometheusCollectRequestDuration())
-
 const app = appBuilder.build()
 
 app.listen(3000)
